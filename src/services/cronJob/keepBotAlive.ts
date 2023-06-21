f11e1f2a8b10f98bad834858b8a74779a0213934
matheoleger <matheo.leger@ynov.com>
2023-06-21 22:41:34 +0200

    feat(stayalive): Change time + add activities

--- src/services/cronJob/keepBotAlive.ts
+++ src/services/cronJob/keepBotAlive.ts
@@ -1,21 +1,25 @@
 import { CronJob } from "cron";
 import * as Discord from "discord.js"
-import { setBotActivity } from "../../utils/discordBotServices";
+import { optioBot, setBotActivity } from "../../utils/discordBotServices";
 
-export const keepBotAliveJob = new CronJob("* */10 * * * *", () => {
+let activityIndexIterator = 0;
 
-    console.log("I try to stay alive")
+export const keepBotAliveJob = new CronJob("0 */10 * * * *", () => {
 
     const activityNames =  [
-      "try to stay alive",
-      "try to bypass free plan",
-      "try to be awesome"
+      "Smoowy coder un autre bot... QUOI ?!",
+      "les magnifiques personnes de ce serveur",
+      "une vidéo sympathique sur internet",
+      "le 473ème épisode de Boblennon sur Skyrim",
+      "le couché du Soleil (il est peut-être que midi)",
+      "rien... ouai... juste rien..."
     ]
   
-    let activityIndex = 0
-  
+    const randomActivityName = activityNames[activityIndexIterator++ % activityNames.length]
+
+
     const activity: Discord.ActivitiesOptions = {
-      name: activityNames[activityIndex++ % activityNames.length],
+      name: randomActivityName,
       url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       type: Discord.ActivityType.Watching
     }
